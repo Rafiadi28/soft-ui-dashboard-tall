0d3ece4c8917fe07392098abf174bc8f32b17846
Rafiadi28 <rafiadi28@gmail.com>
2024-11-05 14:05:44 +0700

    footer

---
+++ resources/views/layouts/footers/guest/footer.blade.php
@@ -0,0 +1,14 @@
+<footer class="py-12">
+    <div class="flex flex-wrap -mx-3">
+      <div class="w-8/12 max-w-full px-3 mx-auto mt-1 text-center flex-0">
+        <p class="mb-0 text-slate-400">
+          Copyright ©
+          <script>
+            document.write(new Date().getFullYear());
+          </script>
+          Soft by <a class="font-semibold text-slate-700" href="https://www.smknukejajar.com" target="_blank">Tim IT SMK NU Kejajar</a>
+        </p>
+      </div>
+    </div>
+  </div>
+</footer>
